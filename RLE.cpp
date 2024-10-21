174bdce9f9a78ee1d8e920ee415e94fc88c8690b
arturmart <artur.mart2002@gmail.com>
2024-10-21 23:35:22 +0400

    Update RLE.cpp

--- RLE.cpp
+++ RLE.cpp
@@ -5,78 +5,79 @@
 
 //Run-length encoding
 
-class RLE{
-    public:
-    
+class RLE {
+public:
+
     //RLE();
-    
-    std::string encode(std::string str){
+
+    std::string encode(std::string str) {
         std::string eC = "";
-        
-        
+
+
         int n = str.length();
-        
+
         int count;
-        
-        for(int i = 0; i<n;i++){
-            
+
+        for (int i = 0; i < n; i++) {
+
             count = 1;
             while (i < n - 1 && str[i] == str[i + 1]) {
                 count++;
                 i++;
             }
-            
-            if(count > 1)  {
-                eC+=std::to_string(count);
-                if(std::isdigit(str[i]))
-                eC+="/";
-                
-                eC+=str[i];
+
+            if (count > 1) {
+                eC += std::to_string(count);
+                //if (std::isdigit(str[i])eC += "/";
+
+                eC += str[i];
 
             }
-            else  eC+=str[i];
+            else  eC += str[i];
         }
-        
-        
-      
+
+
+
         return eC;
     }
-    std::string decode(std::string str){
+    std::string decode(std::string str) {
         std::string dC = "";
         std::string buf = "";
-        
-        
+
+
         int n = str.length();
-        for(int i = 0; i<n;i++){
-            
-            if(!std::isdigit(str[i])) dC+=str[i];
-            else{
+        for (int i = 0; i < n; i++) {
+
+            if (!std::isdigit(str[i])) dC += str[i];
+            else {
                 buf = "";
-                while(std::isdigit(str[i])){
+                while (std::isdigit(str[i])) {
                     buf += str[i];
                     i++;
                 }
-                 std::cout << std::stoi(buf) <<std::endl;
-                dC+=buf;
-                
+
+                for(int j =0; j< std::stoi(buf);j++){
+                dC += str[i];
+                }
+
             }
-            
-            
-           
+
+
+
         }
-        
+
         return dC;
     }
-    
-    
+
+
 };
-int main(){
- std::string str = "Hello World AAAAAAAAAAAA 44444444444444444888888888";
- RLE myRLE;
- std::string eC = myRLE.encode(str);
- std::cout << str <<std::endl;
- std::cout << eC <<std::endl;
-  std::cout<< myRLE.decode(eC) <<std::endl;
- 
- 
-}    
+int main() {
+    std::string str = "Heeeeeellooo Worlddddddddd AAAAAAAAAAAA";
+    RLE myRLE;
+    std::string eC = myRLE.encode(str);
+    std::cout << str << std::endl;
+    std::cout << eC << std::endl;
+    std::cout << myRLE.decode(eC) << std::endl;
+
+
+}
